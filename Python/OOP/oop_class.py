915968ddaf8500b11d8488ac9bed5df616e50b78
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-09-28 09:53:58 +0200

    learning classes creation

---
+++ Python/OOP/oop_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Robot:
+    pass
+x = Robot()
+y = Robot()
+x.name = "Marvin"
+x.build_year = "1958"
+y.name = "shalon"
+y.build_year = "1980"
+print(x.name)
+print(x.__dict__)
---
+++ Python/OOP/sample1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+class Person:
+    def say_hi(self):
+        print("Hello")
+Person().say_hi()
