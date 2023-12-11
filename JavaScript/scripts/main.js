ffd30ef1d0eb0c6814db6305c86f186c1d586688
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2023-12-11 11:39:49 +0200

    updates

--- JavaScript/scripts/main.js
+++ JavaScript/scripts/main.js
@@ -1,2 +1,10 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/DSC_7370 (6).jpg") {
+        myImage.setAttribute("scr", "images/DSC_7370 (4).jpg");
+    } else {
+        myImage.setAttribute("scr", "images/DSC_7370 (6).jpg");
+    }
+};
