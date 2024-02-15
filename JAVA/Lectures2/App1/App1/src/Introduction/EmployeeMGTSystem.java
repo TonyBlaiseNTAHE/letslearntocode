06b74a193fa8829d4b9eeaf2614ebbf071a6201d
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-02-15 08:00:05 +0200

    adding updates

--- JAVA/Lectures2/App1/App1/src/Introduction/EmployeeMGTSystem.java
+++ JAVA/Lectures2/App1/App1/src/Introduction/EmployeeMGTSystem.java
@@ -1,11 +1,15 @@
 package Introduction;
 
+import java.sql.*;
 import java.util.Scanner;
 
 public class EmployeeMGTSystem {
     public static void main(String[] args) {
         // declaration and initialization
         boolean condition = true;
+        final String URL = "jdbc:mysql://localhost:3306/Employee";
+        final String username = "root";
+        final String password = "%password%";
 
         while (condition) {
             System.out.println("*******************");
@@ -22,7 +26,27 @@ public class EmployeeMGTSystem {
             int choice = sc.nextInt();
             switch (choice) {
                 case 1:
-                    System.out.println("Register Employee Selected");
+                    System.out.println("Enter Employee ID: ");
+                    int empId = sc.nextInt();
+                    System.out.println("Enter Employee names: ");
+                    String empName = sc.next();
+                    // steps jdbc;  
+                    // create a connection
+                    try {
+                        Connection con = DriverManager.getConnection(URL, username, password);
+                        Statement st = con.createStatement();
+                        String query = "INSERT INTO Software_engr (id, name) VALUES (" + empId + ", '" + empName + "')";
+
+                        boolean res = st.execute(query);
+                        if (res) {
+                            System.out.println("saved");
+                        } else {
+                            System.out.println("Not saved");
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+
                     break;
                 case 2:
                     System.out.println("Update an Employee");
@@ -32,9 +56,18 @@ public class EmployeeMGTSystem {
                     break;
                 case 0:
                     System.out.println("Thank you for using the system");
-                    System.exit(-1);
+                    System.exit(0);
                     break;
                 default:
+                    System.out.println("Wrong Choice!!! Do you wish to continue");
+                    System.out.println("Type yes to Continue or No to quit");
+                    String answer = sc.next();
+                    if (answer.equalsIgnoreCase("yes")) {
+                        condition = true;
+                    } else {
+                        System.out.println("Thank you for using the System");
+                        condition = false;
+                    }
                     break;
             }
         }
